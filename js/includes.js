a1235e88405a98904f8a494271d6448bdf8825e5
Ethan Voth <ethanvoth1@gmail.com>
2022-06-07 23:06:59 -0600

    Better privacy policy page, and paths now absolute

---
+++ js/includes.js
@@ -0,0 +1,13 @@
+// Loads header and footer, then scrolls to the correct anchor if one is given
+// (loading jQuery content on top of scroll position messes it up, hence this approach)
+$(function () {
+    $("#header").load("/includes/header.html", function () {
+        // Scrolls to the correct position when loaded
+        if (window.location.hash) {
+            $('html, body').scrollTop($(window.location.hash).offset().top);
+        } else {
+            $('html, body').scrollTop(0);
+        };
+    });
+    $("#footer").load("/includes/footer.html");
+});
